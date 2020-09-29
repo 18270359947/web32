57fbe12ccce89bc50c70b65037bafa6cb30858c2
阮建谱 <3261575507@qq.com>
2020-09-29 11:36:37 +0800

    第五次课

--- 第二周课程/第五次课.html/index.js
+++ 第二周课程/第五次课.html/index.js
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-var target = document.getElementById("btn");
-target.onclick = function(){
-     alert("我被点击了")
-=======
 var target = document.getElementById("submit");
         target("我被点击了")
->>>>>>> 4c76414debb528bd4a0736ed913ee4ee1d7ddb4c
