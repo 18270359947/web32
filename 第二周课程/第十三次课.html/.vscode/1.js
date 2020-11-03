c8e64296c85ef1237b1529a327115c1bd9f77ce2
18270359947 <rjp3261575507@qq.com>
2020-11-03 11:38:03 +0800

    js

---
+++ 第二周课程/第十三次课.html/.vscode/1.js
@@ -0,0 +1,9 @@
+function apple(){
+    //var let
+    var array = [12,67,89,100,24,45,67,89];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+apple();
