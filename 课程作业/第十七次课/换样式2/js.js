5d8dad001a5f6a6d7091a4f9d24516d8d212b70d
18270359947 <rjp3261575507@qq.com>
2020-11-18 18:38:02 +0800

    第十七次课

---
+++ 课程作业/第十七次课/换样式2/js.js
@@ -0,0 +1,9 @@
+var apple = function (){
+    console.log('error');
+}
+function orange(){
+    console.log('error orange');
+    function redorange(){
+        console.log('error orange');
+    }
+}
